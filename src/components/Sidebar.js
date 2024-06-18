dbd8de7527a6fa400c13f86e135e7b636c446e6c
HasinMahir77 <130781004+HasinMahir77@users.noreply.github.com>
2024-06-19 01:07:16 +0600

    Active buttons implemented in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,34 +1,79 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./Sidebar.css"; // Import the CSS file for the sidebar
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 function Sidebar({ authenticateFunction }) {
+  const [activeButton,setActiveButton] = useState("dashboardButton");
   const navigate = useNavigate();
-  function handleLogoutButton() {}
-  function handleAboutButton() {
-    navigate("/about");
+
+  function handleDashboardButton() {
+    navigate("/");
+    setActiveButton("dashboardButton");
   }
-  function handleHomeButton() {
+ 
+  function handleVaccinationButton() {
+    setActiveButton("vaccinationButton");
     navigate("/");
   }
-  function handleLogoutButton() {
-    authenticateFunction(false);
-    navigate("/");
+  function handleProfileButton() {
+    setActiveButton("profileButton");
+
   }
+  function handleMedicalDataButton(){
+    setActiveButton("medicalDataButton");
+
+  }
+   function handleLogoutButton() {
+     authenticateFunction(false);
+     navigate("/");
+   }
+
   return (
     <div className="sidebar">
-      <img
-        className="logo"
-        src={require('../images/vaccineLogo.png')}
-      />
-      <button className="homeButton" onClick={handleHomeButton}>
+      <img className="logo" src={require("../images/vaccineLogo.png")} />
+      <button
+        className={
+          activeButton === "dashboardButton"
+            ? "activeButton dashboardButton"
+            : "dashboardButton"
+        }
+        onClick={handleDashboardButton}
+      >
         Dashboard
       </button>
-      <button>Vaccination</button>
-      <button onClick={handleAboutButton}>Profile</button>
-      <button>Medical Data</button>
+      <button
+        onClick={handleVaccinationButton}
+        className={
+          activeButton === "vaccinationButton"
+            ? "activeButton vaccinationButton"
+            : "vaccinationButton"
+        }
+      >
+        Vaccination
+      </button>
+      <button
+        className={
+          activeButton === "profileButton"
+            ? "activeButton profileButton"
+            : "profileButton"
+        }
+        onClick={handleProfileButton}
+      >
+        Profile
+      </button>
+      <button
+        className={
+          activeButton === "medicalDataButton"
+            ? "activeButton medicalDataButton"
+            : "medicalDataButton"
+        }
+        onClick={handleMedicalDataButton}
+      >
+        Medical Data
+      </button>
       <button className="logoutButton" onClick={handleLogoutButton}>
         Logout
       </button>
